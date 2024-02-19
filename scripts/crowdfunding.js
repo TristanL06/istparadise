da288906d8f9ada15f7b9bddd3cd9f250d17992e
TristanL06 <tristan.larguier@gmail.com>
2024-02-19 16:47:17 +0100

    add pertners and starters

--- scripts/crowdfunding.js
+++ scripts/crowdfunding.js
@@ -1,12 +1,16 @@
+let fullSize = true;
+
 function Scroll() {
     var scrollTop = window.scrollY || document.documentElement.scrollTop;
-    if (scrollTop > 100) {
+    if (scrollTop > 100 && fullSize) {
+        fullSize = false;
         document.getElementById("ulule").classList.add("small");
         //change image
         document.getElementById("ulule_img").src = "resources/ulule_square.png";
         document.getElementById("support_title").textContent = "Support us!";
         document.getElementById("ulule").classList.remove("large");
-    } else {
+    } else if (scrollTop <= 100 && !fullSize){
+        fullSize = true;
         document.getElementById("ulule").classList.add("large");
         //change image
         document.getElementById("ulule_img").src = "resources/ulule_plain.png";
